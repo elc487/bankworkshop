9a6a13e64cfbe304472afbc4393a6a7a45932690
Hyaline <charles.eldwin@gmail.com>
2024-01-06 19:51:06 -0500

    testrepo

---
+++ src/app/App.jsx
@@ -0,0 +1,20 @@
+import TransactionHistory from "../features/transactions/TransactionHistory";
+import Transactions from "../features/transactions/Transactions";
+import { Provider } from "react-redux";
+import { store} from "./store";
+import "./app.css";
+
+// TODO: Import the Redux store and provide it to this component using <Provider>.
+const App = () => {
+  return (
+     <Provider store={store}>
+    <main>
+      <h1>Bank Account</h1>
+      <Transactions />
+      <TransactionHistory />
+    </main>
+    </Provider>
+  );
+};
+
+export default App;
